87ace702910e083d06b948de4e2e41d50d5c9097
Kyle Qi <kyle.et.qi@gmail.com>
2025-06-12 01:30:00 -0400

    Add doxygen stuff

--- lib/QMC5883L/QMC5883L.h
+++ lib/QMC5883L/QMC5883L.h
@@ -1,16 +1,44 @@
 #ifndef QMC5883L_H
 #define QMC5883L_H
 
-class QMC5883L {
-    QMC5883L();
-    void calibrate();
+class qmc5883l {
+    public:
+
+    /**
+     * 
+     */
+    qmc5883l();
+
+    /**
+     * 
+     * @param calibrationTime calibration is complete when no new values for this amount of time
+     * 
+     */
+    void calibrate(int calibrationTime);
+
+    /**
+     * 
+     */
     void init();
+
+    /**
+     * 
+     */
     int getX();
+
+    /**
+     * 
+     */
     int getY();
     int getZ();
     float getXNormalized();
     float getYNormalized();
     float getZNormalized();
+    float temperature(); // TODO: Determine return type of temp sensor
+
+    private:
+    int getReading();
+    float normalize(float val);
 };
 
 #endif
