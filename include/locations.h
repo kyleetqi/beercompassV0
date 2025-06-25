644ba20cf7b54eb2f40516fa45a76f17eda8952d
Kyle Qi <kyle.et.qi@gmail.com>
2025-06-25 00:22:37 -0400

    Update file

---
+++ include/locations.h
@@ -0,0 +1,10 @@
+#ifndef LOCATIONS_H
+#define LOCATIONS_H
+
+// Optional: Add this file to .gitignore
+// The following data set is the coordinates of every LCBO
+
+// TODO: Get location of every LCBO
+const float LOCATIONS[10][2] = {0};
+
+#endif
