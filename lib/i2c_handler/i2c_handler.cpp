c58e2f9da93a121b0da3cc5ed25cfa29306e0c2b
Kyle Qi <kyle.et.qi@gmail.com>
2025-06-27 23:36:00 -0400

    Create write function

--- lib/i2c_handler/i2c_handler.cpp
+++ lib/i2c_handler/i2c_handler.cpp
@@ -20,7 +20,7 @@ bool i2cScan(){
       deviceDetected = 1;
       }
     }
-    
+
     return deviceDetected;
 }
 
@@ -28,8 +28,11 @@ bool i2cInit(int sdaPin, int sclPin, uint32_t frequency){
     return Wire.begin(sdaPin, sclPin, frequency);
 }
 
-bool i2cWrite(){
-    return 0;
+bool i2cWrite(uint8_t addr, uint8_t reg, uint8_t data){
+    Wire.beginTransmission(addr);
+    Wire.write(reg);
+    Wire.write(data);
+    return Wire.endTransmission() == 0;
 }
 
 uint8_t i2cRead(uint8_t addr, uint8_t reg){
@@ -39,7 +42,7 @@ uint8_t i2cRead(uint8_t addr, uint8_t reg){
         return 0; // TODO: need to figure out a failed read return
     }
 
-    uint8_t numBytes = 1; // Need dummy variable to get rid of warning
+    uint8_t numBytes = 1; // Need dummy variable to get rid of warning :/
     Wire.requestFrom(addr, numBytes);
     if(Wire.available()){
         return Wire.read();
--- lib/i2c_handler/i2c_handler.h
+++ lib/i2c_handler/i2c_handler.h
@@ -5,7 +5,7 @@
 
 bool i2cScan();
 bool i2cInit(int sdaPin, int sclPin, uint32_t frequency);
-bool i2cWrite();
+bool i2cWrite(uint8_t addr, uint8_t reg, uint8_t data);
 uint8_t i2cRead(uint8_t addr, uint8_t reg);
 
 
