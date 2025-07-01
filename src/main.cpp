287d013c20bb66a5aec7bae1f019726b15dc5204
Kyle Qi <kyle.et.qi@gmail.com>
2025-07-01 15:10:49 -0400

    Update main

--- src/main.cpp
+++ src/main.cpp
@@ -6,17 +6,27 @@
 #include "mpu6500.h"
 #include "neo_6m.h"
 
-// // Declare OLED parameters
-// #define OLED_ADDRESS 0x3c
-// #define SCREEN_WIDTH 128
-// #define SCREEN_HEIGHT 64
-// #define OLED_RESET -1
+// OLED global declarations
+#define OLED_ADDR 0x3c
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+#define OLED_RESET -1
 
-// Global variables
-#define LED_PIN 2 // Devboard LED
+// I2C global declarations
 #define SDA_PIN 21 // I2C SDA pin
 #define SCL_PIN 22 // I2C SCL pin
 #define I2C_FREQ 100000 // I2C clock speed, standard mode (HZ)
+
+// Magnetometer global declarations
+#define QMC5883L_ADDR 0x2C // I2C address of qmc5883l
+#define QMC5883L_MODE 0b11 // Continuous read mode
+#define QMC5883L_ODR 0b10 // 100 Hz output rate
+#define QMC5883L_OSR1 0b10 // 2x oversampling ratio
+#define QMC5883L_OSR2 0b10 // 4x downsampling rate
+// ^tbh all these declarations might not be necessary but we'll see ig
+
+// Create magnetometer object
+qmc5883l myCompass = qmc5883l(QMC5883L_ADDR);
 
 // Keeps track of setup errors
 bool setupSuccessful = true;
@@ -37,7 +47,7 @@ void setup() {
 
   // Reading from register 00 of magnetometer
   // Should output 80 in hex
-  Serial.println(i2cRead(0x2C, 0x00), HEX);
+  Serial.println(i2cRead(QMC5883L_ADDR, 0x00), HEX);
 
 }
 
@@ -46,4 +56,13 @@ void loop() {
   if(!setupSuccessful){
     while(true){}
   }
+
+  // // Testing to see if this actually works
+  if(myCompass.isDRDY() == 1){
+    Serial.println(myCompass.getX(), HEX);
+  } else {
+    Serial.println("Data is not ready!");
+  }
+
+  delay(500);
 }
