69b546215352b7620a97ece53ada1f202906b35c
Kyle Qi <kyle.et.qi@gmail.com>
2025-07-01 14:45:18 -0400

    Cleanup file

--- src/utils.cpp
+++ src/utils.cpp
@@ -6,22 +6,3 @@ float trueHeading(int azimuth, float x, float y, float z){
   return 1.0;
   // TODO: Make this function work
 }
-
-
-void initDisplay() {
-  delay(250);
-  // display.setTextColor(SH110X_WHITE);
-  // display.begin(i2c_Address, true);
-  // display.setContrast(0); 
-  // display.display();
-  // delay(1000);
-  // display.clearDisplay();
-}
-
-void initMagnetometer() {
-
-}
-
-void initAcclerometer() {
-
-}
