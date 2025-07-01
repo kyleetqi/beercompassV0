09f9017c1ef76ed1fcef0c53283b54b013525f40
Kyle Qi <kyle.et.qi@gmail.com>
2025-07-01 14:45:07 -0400

    Create setMode and setOutputRate

--- lib/qmc5883l/qmc5883l.cpp
+++ lib/qmc5883l/qmc5883l.cpp
@@ -1,11 +1,71 @@
 #include "qmc5883l.h"
-#include <Arduino.h>
-#include <Wire.h>
+#include "i2c_handler.h"
+#include "bit_ops.h"
+
+bool qmc5883l::setMode(Mode mode){
+    uint8_t current = i2cRead(this->address, CTRLA_REG);
+
+    // Early return if mode is already set properly
+    if(readBits(current, 0b11) == static_cast<uint8_t>(mode)){
+        return true;
+    }
+
+    // Mode must be set to suspend between different modes
+    if (readBits(current, 0b11) != 0b00){
+        current = writeBits(current, 0b00, 0b11);
+        if (!i2cWrite(this->address, CTRLA_REG, current)){
+            return false;
+        }
+        delay(100);
+    }
+
+    // Write to registers
+    current = writeBits(current, static_cast<uint8_t>(mode), 0b11);
+    return i2cWrite(this->address, CTRLA_REG, current);
+}
+    
+bool qmc5883l::setOutputRate(uint8_t odr){
+
+    // Set bits based on mode selected
+    uint8_t bits;
+    if(odr == 10){
+        bits = 0b00;
+    }else if (odr == 50){
+        bits = 0b01;
+    }else if (odr == 100){
+        bits = 0b10;
+    }else if (odr == 200){
+        bits = 0b11;
+    }else {
+        return false;
+    }
+
+    // Write to register
+    return i2cWrite(this->address, CTRLA_REG, bits);
+
+}
+
+bool setOverSampleRate(uint8_t osr1){
+
+}
+
+bool setDownSampleRate(uint8_t osr2){
+
+}
+
+bool setSetResetMode(uint8_t mode){
+
+}
+
+bool setRange(uint8_t rng){
+
+}
+
+bool resetRegisters(){
+
+}
 
 qmc5883l::CalibrationData qmc5883l::calibrate(int calibrationTime){
-
-    bool isCalibrated = false;
-
 
     // Stores the maximum magnetometer reading
     // Plus the time it was recorded at
@@ -17,6 +77,7 @@ qmc5883l::CalibrationData qmc5883l::calibrate(int calibrationTime){
 
     // If no new max values have been written for
     // 5 seconds, then the compass is calibrated
+    bool isCalibrated = false;
     while (!isCalibrated) {
 
         uint32_t now = millis();
@@ -44,23 +105,24 @@ qmc5883l::CalibrationData qmc5883l::calibrate(int calibrationTime){
 
 }
 
-int qmc5883l::getReading(){
-    return 1;
+bool qmc5883l::isDRDY(){
+    return (i2cRead(this->address, STATUS_REG) & 1);
 }
 
-int qmc5883l::getX() {
-    // TODO: create function
-    return 1;
+bool isOVFL(){
+
 }
 
-int qmc5883l::getY() {
-    // TODO: create function
-    return 1;
+uint16_t qmc5883l::getX() {
+    int16_t result = i2cRead(this->address, XMSB_REG) << 8;
+    result |= i2cRead(this->address, XLSB_REG);
+    return result;
 }
 
-int qmc5883l::getZ() {
-    // TODO: create function
-    return 1;
+uint16_t qmc5883l::getY() {
+}
+
+uint16_t qmc5883l::getZ() {
 }
 
 float qmc5883l::normalize(float val) {
