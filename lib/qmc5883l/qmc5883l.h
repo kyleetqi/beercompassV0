2eb96b623955bb6e4785c7523d62460df459ee8b
Kyle Qi <kyle.et.qi@gmail.com>
2025-07-02 17:01:22 -0400

    Add doxygen

--- lib/qmc5883l/qmc5883l.h
+++ lib/qmc5883l/qmc5883l.h
@@ -17,6 +17,10 @@
 class qmc5883l {
     public:
 
+    /**
+     * @brief Class constructor.
+     * @param myAddr The I2C address of the object.
+     */
     qmc5883l(uint8_t myAddr){
         address = myAddr;
     };
@@ -43,53 +47,76 @@ class qmc5883l {
     bool setOutputRate(uint8_t odr = 100);
 
     /**
-     * @brief Sets the magnetometer's over sample rate.
-     * @param 
+     * @brief Sets the magnetometer's over sample rate ratio
+     * @param osr1 Over sample rate ratio. Valid values: 1, 2, 4, 8. Default value is 2.
+     * @return true if the configuration is successful, false otherwise.
      */
     bool setOverSampleRate(uint8_t osr1 = 2);
+    
+    /**
+     * @brief Sets the magnetometer's down sample rate ratio.
+     * @param osr2 Down sample rate ratio. Valid values: 1, 2, 4, 8. Default value is 4.
+     * @return true if the configuration is successful, false otherwise.
+     */
     bool setDownSampleRate(uint8_t osr2 = 4);
 
+    // TODO: This lol
+    // Make sure the default value makes sense
     bool setSetResetMode(uint8_t mode = 0b01);
-    bool setRange(uint8_t rng = 0b11);
+
+    /**
+     * @brief Sets the magnetometer's magnetic range.
+     * @param rng Magnetic range in Gauss. Valid values: 2, 8, 12, 30. Default value is 2.
+     * @return true if the configuration is successful, false otherwise.
+     */
+    bool setRange(uint8_t rng = 2);
+
+    /**
+     * @brief Resets the magnetometer's registers to its default values.
+     * @return true if the operation is successful, false otherwise.
+     */
     bool resetRegisters();
 
     /** 
-     * @brief Tells you if the magnetometer has data ready
-     * @return The status of the magnetometer
+     * @brief Tells you if the magnetometer has data ready.
+     * @return true if the magnetometer has new data ready, false otherwise.
      */
     bool isDRDY();
 
+    // TODO: Create Doxygen
     bool isOVFL();
 
     /** 
-     * @brief Calculates the maximum readings in the X, Y, and Z axis
-     * @param calibrationTime calibration is complete when no new values for this amount of time
-     * 
+     * @brief Determines and stores the maximum and minimum readings in the X, Y, and Z directions.
+     * @param calibrationTime TBD.
      */
     void calibrate(int calibrationTime);
 
     /** 
-     * @brief Obtains the magnetometer's reading in the X axis
-     * 
+     * @brief Obtains the magnetometer's raw reading in the X axis.
+     * @return The magnetometer's raw reading in the X axis.
      */
     int16_t getXRaw();
 
     /** 
-     * @brief Obtains the magnetometer's reading in the Y axis
-     * 
+     * @brief Obtains the magnetometer's raw reading in the Y axis.
+     * @return The magnetometer's raw reading in the Y axis.
      */
     int16_t getYRaw();
 
     /**
-    * @brief Obtains the magnetometer's reading in the Z axis
-    *
+    * @brief Obtains the magnetometer's raw reading in the Z axis.
+    * @return The magnetometer's raw reading in the Z axis.
     */
     int16_t getZRaw();
 
     float getX();
     float getY();
     float getZ();
-    float getTemperature(); // TODO: Determine return type of temp sensor
+
+    // TODO: Determine return type of temp sensor.
+    // Also idk if the qmc5883l actually has a temp sensor.
+    float getTemperature();
 
     // I don't think these functions are necessary
     // void setMaxX(int16_t val){this->maxX = val;}
