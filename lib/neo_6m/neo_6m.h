5b889a013a1f88410d31b923851d6e69d7386cf5
Kyle Qi <kyle.et.qi@gmail.com>
2025-08-16 21:40:21 -0400

    Add functions

--- lib/neo_6m/neo_6m.h
+++ lib/neo_6m/neo_6m.h
@@ -6,18 +6,37 @@
 class Neo6M {
     public:
 
-    Neo6M(HardwareSerial &serial, int baud = 9600, int rxPin, int txPin);
+    Neo6M(HardwareSerial &serial, uint16_t baud = 9600, uint8_t rxPin, uint8_t txPin);
 
     void begin();
-    void read();
+    bool read();
 
-    double getLatitude();
-
+    float getHour() {return this->hour;}
+    float getMinute() {return this->minute;}
+    float getSecond() {return this->second;}
+    float getLatitude() {return this->latitude;}
+    float getLongitude() {return this->longitude;}
+    float getHorizontalDilution() {return this->horizontalDilution;}
+    float getAltitude() {return this->altitude;}
+    float getGeoidHeight() {return this->geoidHeight;}
+    float getNumSatelites() {return this->numSatelites;}
+    
     private:
 
     HardwareSerial &serial;
-    int baudRate;
-    int rx, tx;
+    uint16_t baudRate;
+    uint8_t rx, tx;
+
+    uint8_t hour, minute, second;
+    float longitude, latitude;
+    float horizontalDilution;
+    float altitude;
+    float geoidHeight;
+    uint8_t numSatelites;
+
+
+
+
 
 };
 
