78b1fdd5196fdd1e781f0c9b0c5790657d867ed8
Kyle Qi <kyle.et.qi@gmail.com>
2025-09-11 15:57:46 -0400

    Init files

---
+++ include/display.h
@@ -0,0 +1,14 @@
+#ifndef DISPLAY_H
+#define DISPLAY_H
+
+#define CENTER_X 64
+#define CENTER_Y 32
+
+/**
+ * @brief TBD
+ * @param azimuth
+ * @param target
+ */
+void drawCompassNeedle(float azimuth, float target);
+
+#endif
