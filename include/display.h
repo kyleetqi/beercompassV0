805b814bccc9eec0cf3afb4a89265461df0043c0
Kyle Qi <kyle.et.qi@gmail.com>
2025-09-15 11:50:38 -0400

    Add display distance

--- include/display.h
+++ include/display.h
@@ -5,11 +5,30 @@
 #include <Adafruit_SSD1306.h>
 
 /**
- * @brief Draws a compass needle pointing towards a target heading
+ * @brief Update's the display's needle direction.
+ * 
  * @param azimuth The user's current heading in degrees, using standard compass convention.
  * @param target The azimuth of the target in degrees, using standard compass convention.
  * @param display Reference to the Adafruit_SSD1306 display object to draw on.
  */
-void updateCompass(float azimuth, float target, Adafruit_SSD1306 &display);
+void drawNeedle(float azimuth, float target, Adafruit_SSD1306 &display);
+
+/**
+ * @brief Updates the display's distance to target.
+ * 
+ * @param distance The distance to the target in meters.
+ * @param display Reference to the Adafruit_SSD1306 display object to draw on.
+ */
+void drawDistance(float distance, Adafruit_SSD1306 &display);
+
+/**
+ * @brief Updates the display's needle direction and distance to target.
+ * 
+ * @param azimuth The user's current heading in degrees, using standard compass convention.
+ * @param target The azimuth of the target in degrees, using standard compass convention.
+ * @param distance The distance to the target in meters.
+ * @param display Reference to the Adafruit_SSD1306 display object to draw on.
+ */
+void updateCompass(float azimuth, float target, float distance, Adafruit_SSD1306 &display);
 
 #endif
--- src/display.cpp
+++ src/display.cpp
@@ -1,18 +1,50 @@
 #include "display.h"
 #include <Adafruit_GFX.h>
+#include <math.h>
 
-void updateCompass(float azimuth, float target, Adafruit_SSD1306 &display){
-    display.clearDisplay();
-    int cx = display.width()/2;
-    int cy = display.height()/2;
+void drawNeedle(float azimuth, float target, Adafruit_SSD1306 &display){
+    int cx = display.width()/2; // X center
+    int cy = display.height()/2; // Y center
     int r = 20; // Length of compass needle
 
     // Converts degrees to radians
     float theta = radians(target-azimuth);
     int x1 = cx + r*cosf(theta);
-    int y1 = cy - r*sinf(theta); // Minus because increments downwards
+    int y1 = cy - r*sinf(theta); // Minus because y increments downwards
 
     display.drawCircle(cx, cy, r, SSD1306_WHITE);
     display.drawLine(cx, cy, x1, y1, SSD1306_WHITE);
+}
+
+void drawDistance(float distance, Adafruit_SSD1306 &display){
+    int cx = display.width()/2;
+    int cy = display.height()/2;
+    int r = 20;
+
+    display.setTextSize(1);
+    display.setTextColor(SSD1306_WHITE);
+    display.setTextWrap(false);
+
+    int roundedDist = (int)round(distance);
+    char buffer[16];
+    snprintf(buffer, sizeof(buffer), "%d m", roundedDist);
+
+    int16_t x, y;
+    uint16_t w, h;
+    display.getTextBounds(buffer, 0, 0, &x, &y, &w, &h);
+
+    int textX = cx-(w/2);
+    int textY = cy + r + 4;
+
+    display.setCursor(textX, textY);
+    display.print(buffer);
+}
+
+void updateCompass(float azimuth, float target, float distance, Adafruit_SSD1306 &display){
+    display.clearDisplay();
+    
+    drawNeedle(azimuth, target, display);
+    drawDistance(distance, display);
+
     display.display();
 }
